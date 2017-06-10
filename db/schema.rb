7634666736df4e4600e8a23a6206284a66381dfc
Gizelle <perasgizelle@gmail.com>
2017-06-10 16:36:18 +0530

    Add message functionality to a chatroom

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606171928) do
+ActiveRecord::Schema.define(version: 20170610101900) do
 
   create_table "chatroom_users", force: :cascade do |t|
     t.integer "chatroom_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170606171928) do
   create_table "messages", force: :cascade do |t|
     t.integer "chatroom_id"
     t.integer "user_id"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
